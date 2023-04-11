dca7ac2199fa19ecd0e8f287e8357d1770e43b7f
Luiz Filipe <99333746+Filipee01@users.noreply.github.com>
2023-04-10 23:33:12 -0300

    add validate

--- main.js
+++ main.js
@@ -10,6 +10,10 @@ const bioText = document.querySelectorAll('.card-text')[2];
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
+  nameValidate()
+  ageValidate()
+  locateValidate()
+  bioValidate()
 
   const name = nameInput.value;
   const age = ageInput.value;
@@ -35,5 +39,57 @@ function previewProfileImage(event) {
       previewImage.src = e.target.result;
     }
     reader.readAsDataURL(input.files[0]);
+  }
+}
+
+
+const form1 = document.querySelector('form')
+const inputss = document.querySelectorAll('.required')
+const spans = document.querySelectorAll('.span-required')
+
+
+function setError(index) {
+    inputss[index].style.border = '2px solid #e63636'
+    spans[index].style.display = 'block'
+}
+
+function removeError(index) {
+  inputss[index].style.border = ''
+  spans[index].style.display = 'none'
+}
+
+function nameValidate() {
+  if(inputss[0].value.length < 3) {
+    setError(0)
+  }
+  else {
+    removeError(0)
+  }
+}
+
+function ageValidate() {
+  if(inputss[1].value.length != 2) {
+    setError(1)
+  }
+  else{
+    removeError(1)
+  }
+}
+
+function locateValidate() {
+  if(inputss[2].value.length < 4 ) {
+    setError(2)
+  }
+  else {
+    removeError(2)
+  }
+}
+
+function bioValidate() {
+  if(inputss[3].value.length < 5 ) {
+    setError(3)
+  }
+  else {
+    removeError(3)
   }
 }
